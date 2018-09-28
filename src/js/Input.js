d355202a6f16f2d959af91cc8685c0316c8b44b4
Yamaki <yusuke.yamaki@rakuten.com>
2018-09-28 14:15:24 +0900

    input

--- src/js/Input.js
+++ src/js/Input.js
@@ -9,9 +9,28 @@ class Input extends React.Component {
             shot : '',
             GB : '',
             miss : ''
-        }
-
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeNumber = this.handleChangeNumber.bind(this);
     }
+    handleSubmit(e){
+        e.preventDefault();
+    }
+    handleChangeNumber(e){
+        this.setState({number: e.target.value});
+    }
+
+    addshot(){
+        this.setState({shot: this.state.shot+1});
+        console.log("aaaa");
+    }
+    addGB(){
+        this.setState({miss: this.state.miss+1});
+    }
+    addmiss(){
+        this.setState({miss: this.state.miss+1});
+    }
+
     render() {
         return (
             <div className="unput">
@@ -24,9 +43,20 @@ class Input extends React.Component {
                         <td>1</td><td>3</td><td>5</td><td>5</td>
                     </tr>
                     <tr>
-                        <td>null</td><td>null</td><td>null</td><td>null</td>
+                        <td>{this.state.number}</td><td>{this.state.shot}</td><td>{this.state.GB}</td><td>{this.state.miss}</td>
                     </tr>
                 </table>
+                <form onSubmit={this.handleSubmit}>
+
+                    <label htmlFor="number">背番号</label>
+                    <input type="text" name="number" value={this.state.number} onChange={this.handleChangeNumber} />
+
+                    <input type="checkbox" name="play" value="shot" onClick={()=>this.addshot} />shot
+                    <input type="checkbox" name="play" value="GB" onClick={()=>this.addGB} />GB
+                    <input type="checkbox" name="play" value="miss" onClick={()=>this.addmiss} />miss
+
+                    <button type="submit">送信</button>
+                </form>
             </div>
         );
     }
